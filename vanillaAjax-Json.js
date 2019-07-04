03a5a7dc6641318d7b0fc992bb42a600b5aeee5d
Uspy <upsylon.dev@gmail.com>
2019-07-04 08:51:53 +0200

    Add - main Script

---
+++ vanillaAjax-Json.js
@@ -0,0 +1,32 @@
+// New Instance of XMLHttpRequest
+
+if (window.XMLHttpRequest)    //  Objet standard
+{ 
+    xhr = new XMLHttpRequest();     //  Firefox, Safari, ...
+} 
+else  if (window.ActiveXObject)      //  Internet Explorer
+{
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+}
+// DOM element recever
+
+let demo = document.getElementById(/*id */)
+let link = document.getElementById(/*triger */)
+
+// event
+link.addEventListener( /*event */ (e) => {
+    e.preventDefault()
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            let data =  JSON.stringify(this.response /* part needed */)
+            
+            // inject
+            demo.innerHTML = data
+        }
+    }
+    xhr.open("GET", "" /*your file  */, true)
+    xhr.responseType = "json"
+    xhr.send( /*prams if needed */ )
+})
+JSON
